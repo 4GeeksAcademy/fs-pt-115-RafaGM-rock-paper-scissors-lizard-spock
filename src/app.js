6dabbfe176e4bb6fdc31087a41c6aa2cd0b1e76a
Rafa-progz <rafa.trabajo@outlook.es>
2025-06-20 19:17:22 +0000

    juego piedra,papel...

--- src/app.js
+++ src/app.js
@@ -1,3 +1,34 @@
-//write your code here
-console.log("Hello Rigo from the console!");
+ // juego en consola con js
 
+ const choose = ["piedra" , "papel" , "tijera" , "lagarto" , "spock"];
+ 
+ const getRandomChoose = () => {
+    const choosing = Math.floor(Math.random() * 5);
+    return choose[choosing]
+ }
+
+ const checkChooseGuess = (userGuess) => {
+     const cpuchoose = getRandomChoose()
+    
+     console.log(`You guessed ${userGuess}`);
+     console.log(`Cpu choose ${cpuchoose}`);
+
+     
+    if (userGuess === "piedra" && (cpuchoose === "tijera" || cpuchoose === "lagarto")){
+        console.log("Ganas");
+    }else if (userGuess === "papel" && (cpuchoose === "piedra" || cpuchoose === "spock")){
+        console.log("Ganas");
+    }else if (userGuess === "tijera" && (cpuchoose === "papel" || cpuchoose === "lagarto")){
+        console.log("Ganas");
+    }else if (userGuess === "lagarto" && (cpuchoose === "papel" || cpuchoose === "spock")){
+        console.log("Ganas");
+    }else if (userGuess === "spock" && (cpuchoose === "tijera" || cpuchoose === "piedra")){
+        console.log("Ganas");
+    }else if (userGuess === cpuchoose){
+        console.log("Empate");
+    }else {
+        console.log("Pierdes");
+ }
+}
+const guess = prompt("Usuario elige a: piedra , papel , tijera , lagarto , spock").toLowerCase()
+checkChooseGuess(guess)
