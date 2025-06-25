a7c61f2afda273f9c18e4029a09f6798681c30ff
Rafa-progz <rafa.trabajo@outlook.es>
2025-06-25 09:46:37 +0000

    areglos con compañeros

--- src/app.js
+++ src/app.js
@@ -1,34 +1,41 @@
- // juego en consola con js
+// juego en consola con js
 
- const choose = ["piedra" , "papel" , "tijera" , "lagarto" , "spock"];
- 
- const getRandomChoose = () => {
-    const choosing = Math.floor(Math.random() * 5);
-    return choose[choosing]
- }
+const choose = ["piedra", "papel", "tijera", "lagarto", "spock"];
+const guess = prompt("Usuario elige a: piedra , papel , tijera , lagarto , spock").toLowerCase();
 
- const checkChooseGuess = (userGuess) => {
-     const cpuchoose = getRandomChoose()
-    
-     console.log(`You guessed ${userGuess}`);
-     console.log(`Cpu choose ${cpuchoose}`);
-
-     
-    if (userGuess === cpuchoose){
-         console.log("Empate");
-    }else if (userGuess === "piedra" && (cpuchoose === "tijera" || cpuchoose === "lagarto")){
-        console.log("Ganas");
-    }else if (userGuess === "papel" && (cpuchoose === "piedra" || cpuchoose === "spock")){
-        console.log("Ganas");
-    }else if (userGuess === "tijera" && (cpuchoose === "papel" || cpuchoose === "lagarto")){
-        console.log("Ganas");
-    }else if (userGuess === "lagarto" && (cpuchoose === "papel" || cpuchoose === "spock")){
-        console.log("Ganas");
-    }else if (userGuess === "spock" && (cpuchoose === "tijera" || cpuchoose === "piedra")){
-        console.log("Ganas");
-    }else {
-        console.log("Pierdes");
- }
+const getRandomChoose = () => {
+	const choosing = Math.floor(Math.random() * choose.length);
+	return choose[choosing]
 }
-const guess = prompt("Usuario elige a: piedra , papel , tijera , lagarto , spock").toLowerCase()
+
+const checkChooseGuess = (userGuess) => {
+	const cpuchoose = getRandomChoose()
+
+	console.log(`You guessed ${userGuess}`);
+	console.log(`Cpu choose ${cpuchoose}`);
+
+	const winner = ["piedra-tijeras", "piedra-lagarto", "papel-piedra", "papel-spock", "tijera-papel", "tijera-lagarto", "lagarto-papel", "lagarto-spock", "spock-tijera", "spock-piedra"];
+	const play = guess + "-" + cpuchoose;
+	if (guess === cpuchoose) console.log("Empate");
+	else if (winner.includes(play)) console.log("Ganas");
+	else console.log("Pierdes");
+
+
+
+	// if (userGuess === cpuchoose) {
+	// 	console.log("Empate");
+	// } else if (userGuess === "piedra" && (cpuchoose === "tijera" || cpuchoose === "lagarto")) {
+	// 	console.log("Ganas");
+	// } else if (userGuess === "papel" && (cpuchoose === "piedra" || cpuchoose === "spock")) {
+	// 	console.log("Ganas");
+	// } else if (userGuess === "tijera" && (cpuchoose === "papel" || cpuchoose === "lagarto")) {
+	// 	console.log("Ganas");
+	// } else if (userGuess === "lagarto" && (cpuchoose === "papel" || cpuchoose === "spock")) {
+	// 	console.log("Ganas");
+	// } else if (userGuess === "spock" && (cpuchoose === "tijera" || cpuchoose === "piedra")) {
+	// 	console.log("Ganas");
+	// } else {
+	// 	console.log("Pierdes");
+	// }
+}
 checkChooseGuess(guess)
